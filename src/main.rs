73a2c073876b46fb5832abfb68d8d3eb78702dde
Markus Grigull <web@grigull.me>
2018-09-17 18:54:53 +0200

    Move subcommand definitions into modules

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@
 extern crate clap;
 extern crate serialport;
 
-use clap::{App, ArgMatches, SubCommand, Arg};
+use clap::{App, ArgMatches};
 
 mod list;
 mod send;
@@ -46,78 +46,12 @@ fn run(matches: ArgMatches) -> Result<(), String> {
 }
 
 fn main() {
-    let databits = [ "5", "6", "7", "8" ];
-    let parity = [ "none", "even", "odd" ];
-    let stopbits = [ "1", "2" ];
-    let flowcontrols = [ "none", "software", "hardware" ];
-
-    let send_subcommand = SubCommand::with_name("send")
-        .about("Send data to serial port")
-        .arg(Arg::with_name("port")
-            .long("port")
-            .short("p")
-            .help("Serial port OS specific name")
-            .required(true)
-            .value_name("PORT")
-            .takes_value(true))
-        .arg(Arg::with_name("baud")
-            .long("baud")
-            .short("b")
-            .help("Serial port baud rate")
-            .takes_value(true)
-            .default_value("9600"))
-        .arg(Arg::with_name("databits")
-            .long("databits")
-            .short("d")
-            .help("Serial port number of data bits")
-            .takes_value(true)
-            .possible_values(&databits)
-            .default_value("8"))
-        .arg(Arg::with_name("parity")
-            .long("parity")
-            .short("P")
-            .help("Serial port parity")
-            .takes_value(true)
-            .possible_values(&parity)
-            .default_value("none"))
-        .arg(Arg::with_name("stopbits")
-            .long("stopbits")
-            .short("s")
-            .help("Serial port stop bits")
-            .takes_value(true)
-            .possible_values(&stopbits)
-            .default_value("1"))
-        .arg(Arg::with_name("flowcontrol")
-            .long("flowcontrol")
-            .short("f")
-            .help("Serial port flow control mode")
-            .possible_values(&flowcontrols)
-            .default_value("none"))
-        .arg(Arg::with_name("echo")
-            .long("echo")
-            .short("e")
-            .help("Echo send text in standard output"))
-        .arg(Arg::with_name("response")
-            .long("show-response")
-            .short("r")
-            .help("Show response from device"))
-        .arg(Arg::with_name("text")
-            .help("Text send to the serial port")
-            .takes_value(true));
-
-    let list_subcommand = SubCommand::with_name("list")
-        .about("List all available serial ports")
-        .arg(Arg::with_name("verbose")
-            .long("verbose")
-            .short("v")
-            .help("Print detailed information about each serial port"));
-
     let matches = App::new("serial-unit-testing")
         .version(crate_version!())
         .version_short("v")
         .about("Serial unit testing framework")
-        .subcommand(send_subcommand)
-        .subcommand(list_subcommand)
+        .subcommand(send::command())
+        .subcommand(list::command())
         .get_matches();
 
     if let Err(e) = run(matches) {
