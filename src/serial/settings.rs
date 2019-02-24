79b9ade026d7cbe382402c09fef563ceda462639
MarkAtk <web@grigull.me>
2019-02-24 13:55:36 +0100

    Add documentation to serial settings

--- src/serial/settings.rs
+++ src/serial/settings.rs
@@ -30,46 +30,69 @@ use std::time::Duration;
 
 use serialport;
 
-#[derive(PartialEq, Debug)]
+/// Number of bits for each data character.
+#[derive(PartialEq, Clone, Eq, Copy, Debug)]
 pub enum DataBits {
+    /// 5 bits per character.
     Five,
+    /// 6 bits per character.
     Six,
+    /// 7 bits per character. Used for true ASCII.
     Seven,
+    /// 8 bits per character. This is default in most cases.
     Eight
 }
 
-#[derive(PartialEq, Debug)]
+/// Data parity check modes.
+#[derive(PartialEq, Clone, Eq, Copy, Debug)]
 pub enum Parity {
+    /// No parity bit used.
     None,
+    /// Parity bit sets for even number of 1 bits.
     Even,
+    /// Parity bit sets for odd number of 1 bits.
     Odd
 }
 
-#[derive(PartialEq, Debug)]
+/// Number of stop bits.
+#[derive(PartialEq, Clone, Eq, Copy, Debug)]
 pub enum StopBits {
+    /// One stop bit.
     One,
+    /// Two stop bits.
     Two
 }
 
-#[derive(PartialEq, Debug)]
+/// Flow control modes.
+#[derive(PartialEq, Clone, Eq, Copy, Debug)]
 pub enum FlowControl {
+    /// No flow control.
     None,
+    /// Flow control using ASCII XON/XOFF bytes.
     Software,
+    /// Flow control using RTS/CTS or DTR/DSR signals.
     Hardware
 }
 
+/// Settings of a serial port connection.
+#[derive(PartialEq, Clone, Eq, Copy, Debug)]
 pub struct Settings {
+    /// Baud rate in bits per second.
     pub baud_rate: u32,
+    /// Timeout duration in milliseconds.
     pub timeout: u64,
-
+    /// Number of data bits.
     pub data_bits: DataBits,
+    /// Parity bit mode.
     pub parity: Parity,
+    /// Number of stop bits.
     pub stop_bits: StopBits,
+    /// Flow control mode.
     pub flow_control: FlowControl
 }
 
 impl Settings {
-    pub fn to_serial_port_settings(&self) -> serialport::SerialPortSettings {
+    pub (super) fn to_serial_port_settings(&self) -> serialport::SerialPortSettings {
         let data_bits = match self.data_bits {
             DataBits::Five => serialport::DataBits::Five,
             DataBits::Six => serialport::DataBits::Six,
