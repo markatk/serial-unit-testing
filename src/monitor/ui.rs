43ba2f586ff7abf91c52d45a2356ba4377e21819
Markus Grigull <web@grigull.me>
2019-08-21 09:06:06 +0200

    Move monitor ui into struct

--- src/monitor/ui.rs
+++ src/monitor/ui.rs
@@ -29,139 +29,165 @@
 use std::io;
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 use tui::Terminal;
 use tui::backend::CrosstermBackend;
 use tui::widgets::{Widget, Block, Borders, Paragraph, Text};
 use tui::layout::{Layout, Constraint, Direction};
-use crossterm::{input, InputEvent, KeyEvent, AlternateScreen, Crossterm};
+use crossterm::{input, InputEvent, KeyEvent, AlternateScreen};
+
+pub fn show_terminal() -> Result<(), io::Error> {
+    let screen = AlternateScreen::to_alternate(true)?;
+    let backend = CrosstermBackend::with_alternate_screen(screen)?;
+    let terminal = Terminal::new(backend)?;
+
+    let mut monitor = Monitor::new(terminal);
+    monitor.run()?;
+
+    Ok(())
+}
 
 enum Event<I> {
     Input(I),
     CursorTick
 }
 
-pub fn show_terminal() -> Result<(), io::Error> {
-    let screen = AlternateScreen::to_alternate(true)?;
-    let backend = CrosstermBackend::with_alternate_screen(screen)?;
-    let mut terminal = Terminal::new(backend)?;
-    terminal.hide_cursor()?;
-
-    // register inputs
-    let (tx, rx) = mpsc::channel();
-    {
-        let tx = tx.clone();
-        thread::spawn(move || {
-            let input = input();
-            let reader = input.read_sync();
-
-            for event in reader {
-                match event {
-                    InputEvent::Keyboard(key) => {
-                        if let Err(_) = tx.send(Event::Input(key.clone())) {
-                            return;
-                        }
-                    },
-                    _ => {}
-                }
-            }
-        });
-    }
-    {
-        let tx = tx.clone();
-        thread::spawn(move || {
-            loop {
-                tx.send(Event::CursorTick).unwrap();
-
-                thread::sleep_ms(500);
-            }
-        });
-    }
-
-    // main loop
-    terminal.clear()?;
-
-    let mut input = String::new();
-    let mut output = String::new();
-    let mut cursor_state = false;
-
-    loop {
-        render(&mut terminal, input.as_str(), output.as_str(), cursor_state)?;
-
-        match rx.recv() {
-            Ok(Event::Input(event)) => {
-                match event {
-                    KeyEvent::Char(c) => {
-                        if c == '\n' {
-                            output.push_str(&input);
-                            output.push('\n');
-
-                            input.clear();
-                        } else {
-                            input.push(c);
-                        }
-                    },
-                    KeyEvent::Ctrl(c) => {
-                        if c == 'c' {
-                            break;
-                        }
-                    },
-                    KeyEvent::Backspace => {
-                        if input.is_empty() == false {
-                            input.pop();
-                        }
-                    },
-                    KeyEvent::Esc => {
-                        break;
-                    },
-                    _ => {}
-                }
-            },
-            Ok(Event::CursorTick) => {
-                cursor_state = !cursor_state;
-            },
-            _ => {}
-        }
-    }
-
-    Ok(())
+struct Monitor {
+    terminal: Terminal<CrosstermBackend>,
+    input: String,
+    output: String,
+    cursor_state: bool
 }
 
-fn render(terminal: &mut Terminal<CrosstermBackend>, input: &str, output: &str, cursor_state: bool) -> Result<(), io::Error> {
-    terminal.draw(|mut f| {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints([
-                Constraint::Min(0),
-                Constraint::Length(3)
-            ].as_ref())
-            .split(f.size());
+impl Monitor {
+    pub fn new(terminal: Terminal<CrosstermBackend>) -> Monitor {
+        Monitor {
+            terminal,
+            input: String::new(),
+            output: String::new(),
+            cursor_state: false
+        }
+    }
 
-        let mut input_text = vec![
-            Text::raw(input)
-        ];
+    pub fn run(&mut self) -> Result<(), io::Error> {
+        // start event threads
+        let (tx, rx) = mpsc::channel();
+        {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                let input = input();
+                let reader = input.read_sync();
 
-        if cursor_state {
-            input_text.push(Text::raw("█"));
+                for event in reader {
+                    match event {
+                        InputEvent::Keyboard(key) => {
+                            if let Err(_) = tx.send(Event::Input(key.clone())) {
+                                return;
+                            }
+                        },
+                        _ => {}
+                    }
+                }
+            });
+        }
+        {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                loop {
+                    tx.send(Event::CursorTick).unwrap();
+
+                    thread::sleep(Duration::from_millis(500));
+                }
+            });
         }
 
-        let output_text = [
-            Text::raw(output)
-        ];
+        // main loop
+        self.terminal.hide_cursor()?;
+        self.terminal.clear()?;
 
-        Paragraph::new(output_text.iter())
-            .block(
-                Block::default()
-            )
-            .wrap(true)
-            .render(&mut f, chunks[0]);
+        loop {
+            self.render()?;
 
-        Paragraph::new(input_text.iter())
-            .block(
-                Block::default()
-                    .title("Input")
-                    .borders(Borders::TOP))
-            .wrap(true)
-            .render(&mut f, chunks[1]);
-    })
+            match rx.recv() {
+                Ok(Event::Input(event)) => {
+                    match event {
+                        KeyEvent::Char(c) => {
+                            if c == '\n' {
+                                self.output.push_str(&self.input);
+                                self.output.push('\n');
+
+                                self.input.clear();
+                            } else {
+                                self.input.push(c);
+                            }
+                        },
+                        KeyEvent::Ctrl(c) => {
+                            if c == 'c' {
+                                break;
+                            }
+                        },
+                        KeyEvent::Backspace => {
+                            if self.input.is_empty() == false {
+                                self.input.pop();
+                            }
+                        },
+                        KeyEvent::Esc => {
+                            break;
+                        },
+                        _ => {}
+                    }
+                },
+                Ok(Event::CursorTick) => {
+                    self.cursor_state = !self.cursor_state;
+                },
+                _ => {}
+            }
+        }
+
+        Ok(())
+    }
+
+    fn render(&mut self) -> Result<(), io::Error> {
+        let input = &self.input;
+        let output = &self.output;
+        let cursor_state = self.cursor_state;
+
+        self.terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints([
+                    Constraint::Min(0),
+                    Constraint::Length(3)
+                ].as_ref())
+                .split(f.size());
+
+            let mut input_text = vec![
+                Text::raw(input)
+            ];
+
+            if cursor_state {
+                input_text.push(Text::raw("█"));
+            }
+
+            let output_text = [
+                Text::raw(output)
+            ];
+
+            Paragraph::new(output_text.iter())
+                .block(
+                    Block::default()
+                )
+                .wrap(true)
+                .render(&mut f, chunks[0]);
+
+            Paragraph::new(input_text.iter())
+                .block(
+                    Block::default()
+                        .title("Input")
+                        .borders(Borders::TOP))
+                .wrap(true)
+                .render(&mut f, chunks[1]);
+        })
+    }
 }
